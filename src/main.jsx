210c70a3311f6cc5fe902858e3e448b0db728c72
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-04-09 21:53:46 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Page Not found</div>,
+    children: [
+      {
+        path: '/',
+        element:<Home />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
