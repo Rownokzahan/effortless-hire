0aa991316e1b57b7b3ef1392d928c1d9c4665959
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-04-11 16:44:25 +0600

    Fix Applied Jobs Component Butoons rendering issue

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -22,24 +22,22 @@ const AppliedJobs = () => {
         <>
             <Banner>Applied Jobs</Banner>
 
-            {
-                appliedJobs.length === 0 ?
-                    <h3 className='text-xl text-center font-bold text-ui-light-gray my-16 md:my-32 mx-2'>You Have Not Applied For Any Jobs Yet!</h3>
+            <section className='mx-2 md:mx-10 lg:mx-20 my-16 md:my-32'>
 
-                    : <section className='mx-2 md:mx-10 lg:mx-20 my-16 md:my-32'>
-
-                        <div className='flex gap-8 items-center justify-end mb-8'>
-                            <h3 className='text-xl font-semibold text-ui-dark'>Filter By : </h3>
-                            <button onClick={filterRemoteJobs} className='bg-ui-light text-ui-purple px-4 py-2 rounded font-semibold shadow border'>Remote</button>
-                            <button onClick={filterOnsiteJobs} className='bg-ui-purple text-ui-light px-4 py-2 rounded font-semibold shadow border'>Onsite</button>
-                        </div>
-
-                        <div className='space-y-4'>
-                            {appliedJobs?.map(job => <AppliedJobCard key={job.id} job={job} />)}
-                        </div>
-                    </section>
-            }
-
+                <div className='flex gap-8 items-center justify-end mb-8'>
+                    <h3 className='text-xl font-semibold text-ui-dark'>Filter By : </h3>
+                    <button onClick={filterRemoteJobs} className='bg-ui-light text-ui-purple px-4 py-2 rounded font-semibold shadow border'>Remote</button>
+                    <button onClick={filterOnsiteJobs} className='bg-ui-purple text-ui-light px-4 py-2 rounded font-semibold shadow border'>Onsite</button>
+                </div>
+               
+                <div className='space-y-4'>
+                    {appliedJobs.length === 0 ?
+                        <h3 className='text-xl text-center font-bold text-ui-light-gray'>You Have Not Applied For Any Jobs Yet!</h3>
+                        : appliedJobs.map(job => <AppliedJobCard key={job.id} job={job} />)                    
+                    }
+                </div>
+                
+            </section>
         </>
     );
 };
